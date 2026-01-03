0b57b5b50585684ed91df3176dfa03f88bc878aa
shiqiooi <159270563+shiqiooi@users.noreply.github.com>
2026-01-03 18:19:37 +0800

    Create case_A_baseline.sql

---
+++ queries/case_A_baseline.sql
@@ -0,0 +1,9 @@
+EXPLAIN (ANALYZE, BUFFERS)
+SELECT c.cityname,
+       COUNT(s.salesid) AS total_transactions
+FROM sales s
+JOIN customers cu ON s.customerid = cu.customerid
+JOIN cities c ON cu.cityid = c.cityid
+GROUP BY c.cityname
+ORDER BY total_transactions DESC
+LIMIT 10;
